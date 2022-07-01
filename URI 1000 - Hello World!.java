00236b1371d0df55f3b54abcd5c82ff50967492e
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 11:22:19 -0300

    Create URI 1000 - Hello World!.java

---
+++ URI 1000 - Hello World!.java
@@ -0,0 +1,14 @@
+// David "Dede" Ribeiro - 2022 - @ddavidi_
+
+import java.io.IOException;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}

a3e1442cfc066a3d42909fd1dbb985840ac0ea5b
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 11:36:24 -0300

    Create URI 1001 - Extremamente Básico.java

---
+++ Java/Iniciante/URI 1001 - Extremamente Básico.java
@@ -0,0 +1,20 @@
+// David "Dede" Ribeiro - 2022 - @ddavidi_
+
+import java.io.IOException;
+import java.util.Scanner; 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		int x = a + b;
+		
+		System.out.println("X = " + x);
+ 
+    }
+ 
+}

532c06907ff952d9b5dda393019c132c4f0dc838
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 12:18:04 -0300

    Create  1004 - Produto Simples.cs

---
+++ C#/1004 - Produto Simples.cs
@@ -0,0 +1,15 @@
+// David "Dede" Ribeiro - 2021 - @ddavidi_
+
+using System;
+
+class URI {
+    
+  static void Main(string[] args) {
+      
+    int valueA = Convert.ToInt32(Console.ReadLine());
+    int valueB = Convert.ToInt32(Console.ReadLine());
+    int SOMA = valueA * valueB;
+    
+    Console.WriteLine("PROD = " + SOMA);
+  }
+}

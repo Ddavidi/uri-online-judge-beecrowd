91e450790e68923447c7a837341b58b7944ac9f5
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 12:19:37 -0300

    Create  1007 - Diferença.cs

---
+++ C#/1007 - Diferença.cs
@@ -0,0 +1,19 @@
+// David "Dede" Ribeiro - 2021 - @ddavidi_
+
+using System;
+
+class URI {
+    
+  static void Main(string[] args) {
+      
+    int valueA = Convert.ToInt32(Console.ReadLine());
+    int valueB = Convert.ToInt32(Console.ReadLine());
+    int valueC = Convert.ToInt32(Console.ReadLine());
+    int valueD = Convert.ToInt32(Console.ReadLine());
+    
+    int diferenca = valueA*valueB - valueC*valueD;
+    
+    Console.WriteLine("DIFERENCA = " + diferenca);
+    
+  }
+}

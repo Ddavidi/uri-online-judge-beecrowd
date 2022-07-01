9266b5daa8094c68a599b4c42b64d2b11e82813a
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 12:23:43 -0300

    Create  1016 - Distância.cs

---
+++ C#/1016 - Distância.cs
@@ -0,0 +1,16 @@
+// David "Dede" Ribeiro - 2021 - @ddavidi
+
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+
+        int distancia = Convert.ToInt32(Console.ReadLine());
+        int tempo = distancia * 2;
+        
+        Console.WriteLine(tempo + " minutos");
+        
+    }
+
+}

14651f666646bdc3f3a5c5dc159bd64d9f1fb691
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-01 12:03:52 -0300

    Create URI 1001 - Extremamente Básico.cpp

---
+++ C++/URI 1001 - Extremamente Básico.cpp
@@ -0,0 +1,19 @@
+// David "Dede" Ribeiro - 2021 - @ddavidi_ 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int A, B, X;
+    
+    cin >> A;
+    cin >> B;
+    
+    X = A + B;
+    
+    cout << "X = " << X << endl;
+
+    return 0;
+}

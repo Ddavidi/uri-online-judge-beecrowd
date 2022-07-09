47730fd2eaef94390115f47c3fc81884da28bf32
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 01:17:00 -0300

    Create 1004 - Produto Simples.c

---
+++ C/1004 - Produto Simples.c
@@ -0,0 +1,19 @@
+/* 
+   ==UserScript==
+ @name         Produto Simples
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1004 - Produto Simples
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <stdio.h>
+main()
+{
+    int A, B, PROD;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    PROD = A * B;
+    printf("PROD = %d\n", PROD);
+}

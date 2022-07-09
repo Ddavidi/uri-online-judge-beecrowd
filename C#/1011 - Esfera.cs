58033786f96ffa6c236fdd132831cf99cd8e529f
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:43:47 -0300

    Update 1011 - Esfera.cs

--- C#/1011 - Esfera.cs
+++ C#/1011 - Esfera.cs
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi
+/* 
+   ==UserScript==
+ @name         Esfera
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1011 - Esfera
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
 
 using System; 
 

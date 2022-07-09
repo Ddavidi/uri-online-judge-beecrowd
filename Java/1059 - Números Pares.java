c0284f33c6d93a565e5d9525933c7e59a8c1b5f4
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 02:24:22 -0300

    Create  1059 - Números Pares.java

---
+++ Java/1059 - Números Pares.java
@@ -0,0 +1,23 @@
+/* 
+   ==UserScript==
+ @name         Números Pares
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1059 - Números Pares
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+import java.util.Scanner;
+import java.io.IOException;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        for(int i=2; i<=100; i = i+2){
+            System.out.println(i);
+        }
+        
+    }
+}

8924e424454ab7816c9bf67564e35a22e074dcd3
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 01:16:20 -0300

    Create 1003 - Soma Simples.c

---
+++ C/1003 - Soma Simples.c
@@ -0,0 +1,19 @@
+/* 
+   ==UserScript==
+ @name         Soma Simples
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1003 - Soma Simples
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <stdio.h>
+main()
+{
+    int A, B, SOMA;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    SOMA = A + B;
+    printf("SOMA = %d\n", SOMA);
+}

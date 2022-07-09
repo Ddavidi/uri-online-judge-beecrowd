2eda277a85ea2a1ca2314489bbce1b99dd879f05
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:52:48 -0300

    Create 1004 - Produto Simples.cpp

---
+++ C++/1004 - Produto Simples.cpp
@@ -0,0 +1,26 @@
+/* 
+   ==UserScript==
+ @name         Produto Simples
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1004 - Produto Simples
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int A, B, PROD;
+    
+    cin >> A >> B;
+    
+    PROD = A * B;
+    
+    cout << "PROD = " << PROD << endl;
+    
+    return 0;
+}

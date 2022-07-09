c702817b0b0f5bbd59d1ab4e4261e2c41435a9a6
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:48:46 -0300

    Update and rename URI 1001 - Extremamente Básico.cpp to 1001 - Extremamente Básico.cpp

---
+++ C++/1001 - Extremamente Básico.cpp
@@ -0,0 +1,27 @@
+/* 
+   ==UserScript==
+ @name         Extremamente Básico
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1001 - Extremamente Básico
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int A, B, X;
+    
+    cin >> A;
+    cin >> B;
+    
+    X = A + B;
+    
+    cout << "X = " << X << endl;
+
+    return 0;
+}

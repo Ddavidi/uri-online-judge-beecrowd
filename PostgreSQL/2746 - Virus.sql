f1a9b2b55cc82d14f284176002d560c4950ce88a
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 03:08:35 -0300

    Create  2746 - Virus.sql

---
+++ PostgreSQL/2746 - Virus.sql
@@ -0,0 +1,12 @@
+/* 
+   ==UserScript==
+ @name         Virus
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 2746 - Virus
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+SELECT REPLACE(name, 'H1', 'X')
+FROM virus

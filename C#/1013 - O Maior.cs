7ccc0b90f491004d7b7725411d3aee8597b3334e
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:44:41 -0300

    Update 1013 - O Maior.cs

--- C#/1013 - O Maior.cs
+++ C#/1013 - O Maior.cs
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi
+/* 
+   ==UserScript==
+ @name         O Maior
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1013 - O Maior
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
 
 using System; 
 

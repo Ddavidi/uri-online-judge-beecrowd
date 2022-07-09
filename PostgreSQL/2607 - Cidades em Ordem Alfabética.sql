89543bb101e4da40b4b9ec1be608c82f20ca0ace
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 02:52:43 -0300

    Create 2607 - Cidades em Ordem Alfabética.sql

---
+++ PostgreSQL/2607 - Cidades em Ordem Alfabética.sql
@@ -0,0 +1,13 @@
+/* 
+   ==UserScript==
+ @name         Cidades em Ordem Alfabética
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 2607 - Cidades em Ordem Alfabética
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+SELECT city
+FROM providers
+ORDER BY city

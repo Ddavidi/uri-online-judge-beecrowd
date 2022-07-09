1b1ba0f791fac01759974c33f4ccfb821d36ecb5
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 01:25:37 -0300

    Create 1016 - Distância.c

---
+++ C/1016 - Distância.c
@@ -0,0 +1,24 @@
+/* 
+   ==UserScript==
+ @name         Distância
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1016 - Distância
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int distancia, minutos;
+  
+  scanf("%d", &distancia);
+  
+  minutos = distancia * 2;
+  
+  printf("%d minutos\n", minutos);
+  
+    return 0;
+}

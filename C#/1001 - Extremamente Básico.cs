b75b785830266906a0f4e7bcc64925ac833f45a1
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:33:59 -0300

    Update 1001 - Extremamente Básico.cs

--- C#/1001 - Extremamente Básico.cs
+++ C#/1001 - Extremamente Básico.cs
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi_
+/* 
+   ==UserScript==
+ @name		    Extremamente Básico
+ @namespace   	https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description 	URI 1001 - Extremamente Básico
+ @copyright   	@ddavidi_
+ @author        @ddavidi_	
+   ==/UserScript==
+*/
 
 using System;
 

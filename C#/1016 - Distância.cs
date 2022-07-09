c569c0fdde2b387f2981303c5fd23626163b71aa
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:46:07 -0300

    Update 1016 - Distância.cs

--- C#/1016 - Distância.cs
+++ C#/1016 - Distância.cs
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi
+/* 
+   ==UserScript==
+ @name         Distância
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1016 - Distância
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
 
 using System; 
 

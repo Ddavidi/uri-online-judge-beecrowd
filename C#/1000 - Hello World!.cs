7abad5378775b58ff02d141f2522a68b235bdbb4
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:36:01 -0300

    Update 1000 - Hello World!.cs

--- C#/1000 - Hello World!.cs
+++ C#/1000 - Hello World!.cs
@@ -1,9 +1,9 @@
 /* 
    ==UserScript==
  @name         Hello World!
- @namespace   	https://github.com/Ddavidi/uri-online-judge-beecrowd
- @description 	URI 1000 - Hello World!
- @copyright   	@ddavidi_
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1000 - Hello World!
+ @copyright    @ddavidi_
  @author       @ddavidi_	
    ==/UserScript==
 */

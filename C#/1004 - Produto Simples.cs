954c262ade29e201e76c517751b26b2f98c5f0b5
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:39:26 -0300

    Update 1004 - Produto Simples.cs

--- C#/1004 - Produto Simples.cs
+++ C#/1004 - Produto Simples.cs
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi_
+/* 
+   ==UserScript==
+ @name         Produto Simples
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1004 - Produto Simples
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
 
 using System;
 

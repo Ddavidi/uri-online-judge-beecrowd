2cd0970efda256fc4c97a16eba876caec0dd4e76
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:51:26 -0300

    Create  1003 - Soma Simples.cpp

---
+++ C++/1003 - Soma Simples.cpp
@@ -0,0 +1,26 @@
+/* 
+   ==UserScript==
+ @name         Soma Simples
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1003 - Soma Simples
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    int A, B, SOMA;
+    
+    cin >> A >> B;
+    
+    SOMA = A + B;
+    
+    cout << "SOMA = " << SOMA << endl;
+    
+    return 0;
+}

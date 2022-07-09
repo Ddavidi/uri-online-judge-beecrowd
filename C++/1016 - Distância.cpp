f83ad70333d2028e7052dfd877704626d7c8b74a
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:50:03 -0300

    Create  1016 - Distância.cpp

---
+++ C++/1016 - Distância.cpp
@@ -0,0 +1,27 @@
+/* 
+   ==UserScript==
+ @name         Distância
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1016 - Distância
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+    
+    int distancia;
+    
+    cin >> distancia;
+    
+    cout << distancia*2 << " minutos" << endl;
+    
+    return 0;
+    
+}

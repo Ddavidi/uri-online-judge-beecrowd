8a9bec2efc1693803e32fce51d1c53e66dd50f53
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 00:47:39 -0300

    Update 1000 - Hello World!.cpp

--- C++/1000 - Hello World!.cpp
+++ C++/1000 - Hello World!.cpp
@@ -1,4 +1,12 @@
-// David "Dede" Ribeiro - 2021 - @ddavidi_
+/* 
+   ==UserScript==
+ @name         Hello World!
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1000 - Hello World!
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
 
 #include <iostream>
 using namespace std;

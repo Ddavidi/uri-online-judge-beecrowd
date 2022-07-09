471674ba3f4a4fc2f28b6b3e4dcbfcd6c78dda19
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 02:49:21 -0300

    Create  2602 - Select Básico.sql

---
+++ PostgreSQL/2602 - Select Básico.sql
@@ -0,0 +1,13 @@
+  /* 
+   ==UserScript==
+ @name         Select Básico
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 2602 - Select Básico
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+SELECT name
+  FROM customers
+  WHERE state = 'RS'

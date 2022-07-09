92b69697ba36bf6049093bcf1ba4174f4621758f
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 01:13:40 -0300

    Update and rename URI 1001 - Extremamente Básico.c to 1001 - Extremamente Básico.c

---
+++ C/1001 - Extremamente Básico.c
@@ -0,0 +1,19 @@
+/* 
+   ==UserScript==
+ @name         Extremamente Básico
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1001 - Extremamente Básico
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+#include <stdio.h>
+int main()
+{
+    int A, B, X;
+    scanf("%d", &A);
+    scanf("%d", &B);
+    X = A + B;
+    printf("X = %d\n", X);
+}

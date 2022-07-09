271a96cf011e3ac31b6d6a4acedf5df31bab34bc
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 01:53:33 -0300

    Update and rename URI 1000 - Hello World!.java to 1000 - Hello World!.java

---
+++ Java/1000 - Hello World!.java
@@ -0,0 +1,22 @@
+/* 
+   ==UserScript==
+ @name         Hello World!
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 1000 - Hello World!
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+import java.io.IOException;
+ 
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        System.out.println("Hello World!");
+ 
+    }
+ 
+}

0df9eea75b8cdfd62d6d03ea07b09eab8bbea815
David Nunes <56649157+Ddavidi@users.noreply.github.com>
2022-07-09 02:56:47 -0300

    Create 2615 - Expandindo o Negocio.sql

---
+++ PostgreSQL/2615 - Expandindo o Negocio.sql
@@ -0,0 +1,12 @@
+/* 
+   ==UserScript==
+ @name         Expandindo o Negocio
+ @namespace    https://github.com/Ddavidi/uri-online-judge-beecrowd
+ @description  URI 2615 - Expandindo o Negocio
+ @copyright    @ddavidi_
+ @author       @ddavidi_	
+   ==/UserScript==
+*/
+
+SELECT DISTINCT city
+FROM customers
